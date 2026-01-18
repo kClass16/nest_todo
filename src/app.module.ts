ec07b5ad41190959ddd89e0a79efb106b0bcb046
kClass16 <kaliyaclasses16@gmail.com>
2026-01-18 18:37:17 +0000

    fix: update MongoDB connection string to include database name

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { TodosModule } from './todos/todos.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://kaliyaclasses16:61lTru3qNCYfPyxj@cluster0.ktao9kg.mongodb.net'),TodosModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://kaliyaclasses16:61lTru3qNCYfPyxj@cluster0.ktao9kg.mongodb.net/nest_todos'),TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
